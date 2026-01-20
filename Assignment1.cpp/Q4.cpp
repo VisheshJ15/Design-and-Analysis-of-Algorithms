cc90d71ac0e530481827a34b1659b2c94411592b
VisheshJ15 <jain.vishesh1093@gmail.com>
2026-01-20 09:13:21 +0530

    Q4.cpp

--- Assignment1.cpp/Q4.cpp
+++ Assignment1.cpp/Q4.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int maxSubArraySum(int arr[], int n) {
-    int currentSum = arr[0];
-    int maxSum = arr[0];
-    for (int i = 1; i < n; i++) {
-        currentSum = max(arr[i], currentSum + arr[i]);
-        maxSum = max(maxSum, currentSum);
+int Maxsubarray(int A[],int p,int q) {
+    if (p==q) {
+        return max(A[p],0);
     }
-    return maxSum;
+    int m=(p+q)/2;
+    int maxL=Maxsubarray(A,p,m);
+    int maxR = Maxsubarray(A,m+1, q);
+    int sum=0;
+    int maxmidl=0;
+    for (int i=m;i>=p;i--) {
+        sum += A[i];
+        maxmidl = max(maxmidl,sum);
+    }
+    sum = 0;
+    int maxmidr = 0;
+    for (int j=m+1;j<=q;j++) {
+        sum += A[j];
+        maxmidr=max(maxmidr,sum);
+    }
+    int maxmid=maxmidl+maxmidr;
+    return max(max(maxL,maxR),maxmid);
 }
 int main() {
-    int arr[] = {-2,-5,6,-2,-3,1,5,-6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << "Maximum subarray sum is: "
-         << maxSubArraySum(arr, n);
-    return 0;
+    int arr[] = {-2,-5,6,-2,-3,1,5,-6};;
+    int result = Maxsubarray(arr,0,7);
+    cout<<result;
 }
